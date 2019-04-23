1217363dd8bfd9c1ef73b2f2e832a010e0aad97d
Maribel Dapeton <48677321+maribel4amido@users.noreply.github.com>
2019-04-23 15:32:22 +0100

    Use CI mode for running tests

---
+++ functions/jest.config.js
@@ -0,0 +1,9 @@
+const { defaults } = require('jest-config');
+module.exports = {
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
+  testMatch: ['**/firestore/**/*unit.test.js', '**/utils/**/*unit.test.js'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  testEnvironment: 'node',
+  verbose: false,
+  reporters: ['default', 'jest-dot-reporter', 'jest-junit'],
+};
