ff4553a40bc1f60403f437ec84a2f5e3e02aad85
Ross Aubrey-Smith <Ross.Aubrey-Smith@amido.com>
2019-05-21 15:04:30 +0100

    remove unused storage test

--- rules/storage/authenticated.test.js
+++
@@ -1,6 +0,0 @@
-// TODO: figure out options for cloud storage rules testing
-xdescribe("Cloud Storage authenticated access", () => {
-  it("should allow reads", async () => {});
-
-  it("should allow writes", async () => {});
-});
--- rules/storage/public.test.js
+++
@@ -1,6 +0,0 @@
-// TODO: figure out options for cloud storage rules testing
-xdescribe("Cloud Storage public access", () => {
-  it("should deny reads", async () => {});
-
-  it("should deny writes", async () => {});
-});
