ef5fa19fd24e907b635d7be33bbb11101b60ece4
Ross Aubrey-Smith <ross.aubrey-smith@amido.com>
2019-07-11 09:28:42 +0100

    FARM-408 integration tests for cloud functions (#16)
    * clean up index.js
    * refactor validation functions
    * add integration test for schema validation
    * update unit tests
    * update lint ecma for object spread support
    * Add integration tests for linking cropscores
    * run emulator test for cloud functions
    * Simplify CI/CD workflow

--- functions/validate/validate_schema.js
+++ functions/validate/validate_schema.js
@@ -1,50 +1,22 @@
 const Ajv = require('ajv');
-const firestore = require('../utils/firestore_repository');
-const slack = require('../utils/slack_alert');
-const fs = require('fs');
-const path = require('path');
 
 const ajv = Ajv({ allErrors: true });
 
-function validateSchema(data) {
-  const { schema, fl_id } = data._fl_meta_;
+const getSchema = schema => {
+  try {
+    const filepath = `../model/schemas/${schema}.schema.json`;
+    return require(filepath);
+  } catch (error) {
+    throw new Error(`Could not find schema. Schema: ${schema}`);
+  }
+};
 
+const validateSchema = (schema, data) => {
   const jsonSchema = getSchema(schema);
-  const valid = ajv.validate(jsonSchema, data);
-  const msg = `Schema validation - doc: ${fl_id}, schema: ${schema}, result: `;
-
-  if (valid) {
-    console.log(`${msg} PASSED`);
-    return firestore.deleteDocument('fs_content_errors', fl_id);
-  } else {
-    console.log(`${msg} FAILED`);
-    slack.post(`${msg} FAILED`);
-    return firestore.writeDocument('fs_content_errors', fl_id, getContentError(data));
-  }
-}
-
-function getSchema(schema) {
-  const filepath = path.resolve(__dirname, `../model/schemas/${schema}.schema.json`);
-  if (fs.existsSync(filepath)) {
-    return JSON.parse(fs.readFileSync(filepath, 'utf8'));
-  } else {
-    const msg = `Could not find schema. Schema: ${schema}`;
-    slack.post(msg);
-    throw new Error(msg);
-  }
-}
-
-function getContentError(data) {
-  return {
-    name: data.name || data.title,
-    fl_id: data._fl_meta_.fl_id,
-    env: data._fl_meta_.env,
-    locale: data._fl_meta_.locale,
-    schema: data._fl_meta_.schema,
-    errors: ajv.errors,
-  };
-}
+  ajv.validate(jsonSchema, data);
+  return ajv.errors;
+};
 
 exports = module.exports = {
-  validateSchema: validateSchema,
+  validateSchema,
 };
--- functions/validate/validate_schema.unit.test.js
+++ functions/validate/validate_schema.unit.test.js
@@ -1,47 +1,4 @@
 const { validateSchema } = require('./validate_schema');
-const firestore = require('../utils/firestore_repository');
-const validDocument = require('../model/json/crop.sample.json');
-const slack = require('../utils/slack_alert');
-
-jest.mock('../utils/firestore_repository');
-jest.mock('../utils/slack_alert');
-
-describe('Validate schema', () => {
-  afterAll(() => {
-    jest.restoreAllMocks();
-  });
-
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
-  it('should write to error log if validation fails', () => {
-    validateSchema(invalidDocument);
-    expect(firestore.writeDocument).toBeCalled();
-  });
-
-  it('should post a slack alert if validation fails', () => {
-    validateSchema(invalidDocument);
-    expect(slack.post).toBeCalled();
-  });
-
-  it('should delete existing error log if validation passes', () => {
-    validateSchema(validDocument);
-    expect(firestore.deleteDocument).toBeCalled();
-  });
-
-  it('should throw an error if no JSON schema is found', () => {
-    expect(() => validateSchema(noSchemaDocument)).toThrow();
-  });
-
-  it('should post a slack alert if no JSON schema is found', () => {
-    try {
-      validateSchema(noSchemaDocument);
-    } catch (error) {
-      expect(slack.post).toBeCalled();
-    }
-  });
-});
 
 const invalidDocument = {
   _fl_meta_: {
@@ -59,3 +16,22 @@ const noSchemaDocument = {
   name: 'tomato',
   status: 'PUBLISHED',
 };
+
+describe('Validate schema', () => {
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should return errors if schema is invalid', () => {
+    const errors = validateSchema('crop', invalidDocument);
+    expect(errors).toBeTruthy();
+  });
+
+  it('should throw an error if no JSON schema is found', () => {
+    expect(() => validateSchema('noShema', noSchemaDocument)).toThrow();
+  });
+});
